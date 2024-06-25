cb46a7871365520790ba639b53748604e23b2445
Bruno José <bruninhosese12@gmail.com>
2024-06-24 23:20:07 -0300

    CHORE: deixando a middleware pré-escrita para ser usada no login

--- app/Http/Middleware/EnsureIsValidSeller.php
+++ app/Http/Middleware/EnsureIsValidSeller.php
@@ -15,7 +15,6 @@ class EnsureIsValidSeller
      */
     public function handle(Request $request, Closure $next): Response
     {
-        dump('passou na middleware');
         return $next($request);
     }
 }
