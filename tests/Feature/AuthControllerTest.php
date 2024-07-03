d4b49f8336eb99a4714acbf2ad8a446a21525550
bruno_jose <bruninhosese12@gmail.com>
2024-07-03 18:02:44 -0300

    FEAT: unit test for authentication

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -1,12 +1,51 @@
 <?php
 
-describe('authentication', function () {
-    it('can make a login', function () {
-        $response = route('login', [
-            'email' => 'user_test@test.com',
-            'password' => '123456'
-        ]);
+use App\Infrastructure\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response as Status;
 
-        dd($response);
+uses(RefreshDatabase::class);
+
+describe('authentication', function () {
+    $credentials = [
+        'email' => 'test@test.com',
+        'password' => '123456'
+    ];
+
+    it('can make a login', function () use ($credentials) {
+        $user = User::factory()->create($credentials);
+
+        $response = app()->handle(Request::create(
+            route('login', $credentials),
+            'POST'
+        ));
+        
+        expect($response)->toBeInstanceOf(JsonResponse::class);
+        expect($response->getStatusCode())
+            ->toBeInt()
+            ->toBe(Status::HTTP_ACCEPTED);
+    });
+
+    it('may remain active', function () use ($credentials) {
+        $user = User::factory()->create($credentials);
+        $token = $user->createToken('test-token')->plainTextToken;
+
+        $request = Request::create(route('authenticated.user'), 'GET');
+        $request->headers->set('Authorization', 'Bearer ' . $token);
+
+        $response = app()->handle($request);
+        
+        expect($response)->toBeInstanceOf(JsonResponse::class);
+        expect($response->getStatusCode())
+            ->toBeInt()
+            ->toBe(Status::HTTP_OK);
+        
+        $userFromResponse = json_decode($response->getContent())->user;
+
+        expect($userFromResponse)->toHaveProperties(
+            array_keys($user->toArray())
+        );
     });
 });
