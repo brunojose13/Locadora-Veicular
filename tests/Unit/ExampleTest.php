016ad2e888ff8f8e679c150a558a9f7b32a3fbcb
Bruno José <bruninhosese12@gmail.com>
2024-07-03 23:05:30 -0300

    FEAT: added unit test for example

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+describe('sum', function () {
+    it('may sum integers', function () {
+        $result = sum(1, 2);
+  
+        expect($result)->toBe(3);
+     });
+  
+     it('may sum floats', function () {
+        $result = sum(1.5, 2.5);
+  
+        expect($result)->toBe(4.0);
+     });
+ })->skip();
+
+function sum($a, $b) 
+{
+    return $a + $b;
+}
+ 
