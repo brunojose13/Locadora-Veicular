6889f8525ad36e085e2dbd08f2f435a4a52213cf
Bruno José <bruninhosese12@gmail.com>
2024-07-04 23:50:56 -0300

    FEAT: created an value object for credentials

---
+++ app/Domain/ValueObjects/Credentials.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Domain\ValueObjects;
+
+class Credentials
+{
+    public function __construct(private string $email, private string $password)
+    {
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'email' => $this->getEmail(),
+            'password' => $this->getPassword()
+        ];
+    }
+}
