f057366d943982e82c14b7a465d4f4b7f6110145
Bruno José <bruninhosese12@gmail.com>
2024-07-07 02:59:47 -0300

    FEAT: created all crud tests in UserRepositoryTest

--- tests/Unit/UserRepositoryTest.php
+++ tests/Unit/UserRepositoryTest.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Domain\Entities\Collections\UserCollection;
 use App\Domain\Entities\User as UserEntity;
 use App\Domain\Ports\User\IUserRepository;
 use App\Domain\ValueObjects\Credentials;
+use App\Domain\ValueObjects\UserData;
 use App\Infrastructure\Models\User;
 
 beforeEach(function () {
@@ -10,20 +12,41 @@ beforeEach(function () {
 });
 
 describe('C.R.U.D for User', function () {
+    it('may list all', function () {
+        $userEntityCollection = $this->userRepository->all();
+
+        expect($userEntityCollection)->toBeInstanceOf(UserCollection::class);
+    });
+
     it('may create', function (User $user) {
         $user->delete();
 
-        $success = $this->userRepository->save(new UserEntity(
-            null,
+        $userEntity = $this->userRepository->save(new UserData(
             $user->name,
             new Credentials($user->email, $user->password),
-            $user->remember_token,
-            $user->created_at,
-            $user->updated_at
         ));
 
+        expect($userEntity)->toBeInstanceOf(UserEntity::class);
+    })->with('user');
+
+    it('may update', function (User $user) {
+        $userEntity = $this->userRepository->update(new UserData(
+            $user->name,
+            new Credentials($user->email, $user->password),
+        ));
+
+        expect($userEntity)->toBeInstanceOf(UserEntity::class);
+    })->with('user');
+
+    it('may search', function (User $user) {
+        $userEntity = $this->userRepository->getById($user->id);
+
+        expect($userEntity)->toBeInstanceOf(UserEntity::class);
+    })->with('user');
+
+    it('may delete', function (User $user) {
+        $success = $this->userRepository->delete($user->id);
+
         expect($success)->toBeTrue();
-    })->with([
-        'model' => fn() => User::factory()->create()
-    ])->todo();
+    })->with('user');
 });
