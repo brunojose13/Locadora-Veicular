845c1dd71da1884fd9a2ca5d39581bbe7de20167
Bruno José <bruninhosese12@gmail.com>
2024-07-07 12:19:12 -0300

    FEAT: setted the type hint

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Helpers;
 
 if (! function_exists('getClassShortName')) {
-    function getClassShortName(object|string $objectOrClass)
+    function getClassShortName(object|string $objectOrClass): string
     {
         $reflection = new \ReflectionClass($objectOrClass);
         
