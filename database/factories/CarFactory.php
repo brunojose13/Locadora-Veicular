4abed9686710f6772141708d3f47b5350b6bd572
Bruno José <bruninhosese12@gmail.com>
2024-07-07 13:09:33 -0300

    FEAT: created a car factory

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Infrastructure\Models\Car;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CarFactory extends Factory
+{
+    protected $model = Car::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'brand' => fake()->text(10),
+            'model' => fake()->word(),
+            'age' => fake()->numberBetween(2019, 2024),
+            'price' =>  fake()->numberBetween(50, 500)
+        ];
+    }
+}
