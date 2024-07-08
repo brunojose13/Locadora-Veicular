6ae8cabff4e2ae0f28640c2917e44310e76b1bfb
Bruno José <bruninhosese12@gmail.com>
2024-07-07 22:17:46 -0300

    FEAT: finished crud test

--- tests/Unit/CarServiceTest.php
+++ tests/Unit/CarServiceTest.php
@@ -2,9 +2,11 @@
 
 use App\Domain\Contracts\Ports\Out\CarListOutput;
 use App\Domain\Contracts\Ports\Out\CarOutput;
+use App\Domain\Contracts\Ports\Out\DeletedCarOutput;
 use App\Domain\Entities\Car as CarEntity;
 use App\Domain\Services\CarService;
 use App\Domain\ValueObjects\CarData;
+use App\Exceptions\CarNotFoundException;
 use App\Infrastructure\Models\Car;
 
 beforeEach(function () {
@@ -13,13 +15,17 @@ beforeEach(function () {
 
 describe('CRUD for Car', function () {
     it('gets all cars', function () {
-        Car::factory()->create();
+        Car::factory(2)->create();
+        
         $outputObject = $this->carService->getCars();
+        $output = $outputObject->getOutput();
 
         expect($outputObject)->toBeInstanceOf(CarListOutput::class);
-        expect($outputObject->getOutput())
-            ->toBeArray()
-            ->not->toBeEmpty();
+        expect($output)->toBeArray();
+
+        expect($output['CarCollection'])
+            ->not->toBeEmpty()
+            ->toHaveCount(2);
     });
 
     it('creates a car', function (CarData $carData) {
@@ -27,9 +33,7 @@ describe('CRUD for Car', function () {
         $output = $outputObject->getOutput();
         
         expect($outputObject)->toBeInstanceOf(CarOutput::class);
-        expect($output)
-            ->toBeArray()
-            ->not->toBeEmpty();
+        expect($output)->toBeArray();
         
         expect($output['Car']['brand'])->toBe('Jeep')
             ->and($output['Car']['model'])->toBe('Renegade');
@@ -47,13 +51,52 @@ describe('CRUD for Car', function () {
         $output = $outputObject->getOutput();
 
         expect($outputObject)->toBeInstanceOf(CarOutput::class);
-        expect($output)
-            ->toBeArray()
-            ->not->toBeEmpty();
+        expect($output)->toBeArray();
         
         expect($output['Car']['brand'])->toBe('Toyota')
             ->and($output['Car']['model'])->toBe('Corolla')
             ->and($output['Car']['age'])->toBe(2021)
             ->and($output['Car']['price'])->toBe((float) 300);
+    })->with('carModel');
+
+    it('throws when updating a non-existent car', function () {
+        $this->carService->updateCar(new CarEntity(
+            1,
+            '',
+            '',
+            0,
+            0
+        ));
+    })->expectException(CarNotFoundException::class);
+
+    it('gets a car by ID', function (Car $car) {       
+        $outputObject = $this->carService->getCarById($car->id);
+        $output = $outputObject->getOutput();
+
+        expect($outputObject)->toBeInstanceOf(CarOutput::class);
+        expect($output)->toBeArray();
+    })->with('carModel');
+
+    it('throws when getting a non-existent car by ID', function () {
+        $this->carService->getCarById(1);
+    })->expectException(CarNotFoundException::class);
+
+    it('deletes a car', function (Car $car) {
+        $outputObject = $this->carService->deleteCar($car->id);
+
+        expect($outputObject)->toBeInstanceOf(DeletedCarOutput::class);
+    })->with('carModel');
+
+    it('throws when deleting a non-existent car', function () {
+        $this->carService->deleteCar(1);
+    })->expectException(CarNotFoundException::class);
+
+    it('gets all deleted cars', function (Car $car) {
+        $car->delete();
+
+        $outputObject = $this->carService->getDeletedCars();
+
+        expect($outputObject)->toBeInstanceOf(CarListOutput::class);
+        expect($outputObject->getOutput())->toBeArray();
     })->with('carModel');
 });
